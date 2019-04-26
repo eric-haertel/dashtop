34f19292b8b346b049dc0a2321d2b22c19c844af
Eric Härtel <eric.haertel@auto1.com>
2019-04-26 17:58:22 +0200

    setup electron

---
+++ index.js
@@ -0,0 +1,12 @@
+const electron= require('electron')
+
+function createWindow() {
+    let win = new electron.BrowserWindow({width: 300, height: 200})
+    win.loadFile('index.html')
+}
+
+electron.app.on("ready", createWindow);
+electron.app.on("window-all-closed", () => {electron.app.quit()});
+console.log('node version ' + process.versions.node);
+console.log('chrome version ' + process.versions.chrome);
+console.log('node electron ' + process.versions.electron);
