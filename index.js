3557be950a51d2ff9ad4b412034540b3b84a90be
Eric Härtel <eric.haertel@auto1.com>
2019-04-29 11:09:08 +0200

    test setup

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function createWindow() {
 
 electron.app.on("ready", createWindow);
 electron.app.on("window-all-closed", () => {electron.app.quit()});
+
 console.log('node version ' + process.versions.node);
 console.log('chrome version ' + process.versions.chrome);
 console.log('node electron ' + process.versions.electron);
---
+++ test/test.js
@@ -0,0 +1,7 @@
+const expect = require('expect.js');
+
+describe('Test', function(){
+    describe('does it work', function() {
+        expect(true).to.be.ok();
+    })
+})
